b66c15c40a08f36624d557b69c34c830481988b3
chawol <chawolbaka@hotmail.com>
2018-09-04 09:14:03 +0800

    add class SystemInfo

---
+++ PlayersMonitor-Console/SystemInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlayersMonitor
+{
+    internal static class SystemInfo
+    {
+#if !DoNet
+        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+#elif Windows
+        public static bool IsWindows { get { return true; } }
+#else
+        public static bool IsWindows { get { return false; } }
+#endif
+    }
+}
